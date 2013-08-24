ef7d1208688a20f1add363a5d0573170f675c04d
Mikko Koski <mikko.koski@futurice.com>
2013-08-24 13:54:01 +0300

    isSome/isNone

--- test/test.js
+++ test/test.js
@@ -7,6 +7,19 @@ var expect = require('expect.js');
 describe('Option', function() {
   it('#valueOf', function() {
     expect(Some('value').valueOf()).to.be.eql('value');
+    expect(Some(null).valueOf()).to.be.eql(null);
     expect(None().valueOf()).to.be.eql(null);
+  });
+
+  it('#isSome', function() {
+    expect(Some('value').isSome()).to.be.eql(true);
+    expect(None().isSome()).to.be.eql(false);
+    expect(Some(null).isSome()).to.be.eql(true);
+  });
+
+  it('#isNone', function() {
+    expect(Some('value').isNone()).to.be.eql(false);
+    expect(None().isNone()).to.be.eql(true);
+    expect(Some(null).isNone()).to.be.eql(false);
   });
 });
